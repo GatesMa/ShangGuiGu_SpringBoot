bfd8f4551eb10fb248ebb492473b5b759fdfed09
GatesMa <gatesma@foxmail.com>
2020-01-12 20:58:25 +0800

    Spring-Boot-增加修改

--- spring-boot-04-web-restfulcrud/src/main/java/com/gatesma/springboot/controller/HelloController.java
+++ spring-boot-04-web-restfulcrud/src/main/java/com/gatesma/springboot/controller/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
+import java.util.Map;
+
 /**
  * Copyright (C), 2020
  * FileName: HelloController
@@ -21,5 +24,14 @@ public class HelloController {
     public String hello() {
         return "Hello";
     }
+
+    @RequestMapping("/success")
+    public String success(Map<String, Object> map) {
+        map.put("hello", "<h1>你好</h1>");
+        map.put("users", Arrays.asList("zhangsan","lisi","wangwu"));
+        return "success";
+    }
+
+
 
 }
