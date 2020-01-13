318cae47e4a5fb74d887e48ac208ecaef2a24c22
GatesMa <gatesma@foxmail.com>
2020-01-13 23:47:56 +0800

    Spring-Boot-增加修改

--- spring-boot-06-data-mybatis/src/main/java/com/gatesma/springboot06/SpringBoot06DataMybatisApplication.java
+++ spring-boot-06-data-mybatis/src/main/java/com/gatesma/springboot06/SpringBoot06DataMybatisApplication.java
@@ -1,8 +1,10 @@
 package com.gatesma.springboot06;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan(value = "com.gatesma.springboot06.mapper")
 @SpringBootApplication
 public class SpringBoot06DataMybatisApplication {
 
